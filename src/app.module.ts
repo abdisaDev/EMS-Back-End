66f49f9b6094fd2a13903c493ab2c08a76194ce1
Abdisa Dev <abdisaDev@gmail.com>
2024-05-03 11:08:48 +0300

    ennnnnnnnnv

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,20 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './typeorm/entities/users/User';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.development.local',
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'abdisa',
-      password: 'Abdisa@2423',
-      database: 'ems',
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [User],
       synchronize: true,
     }),
